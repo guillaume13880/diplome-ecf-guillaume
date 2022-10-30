a508dc99b6c4bfc214d2192b36156fa311938e58
guillaume <guillaume.laurent13880@gmail.com>
2022-10-30 19:49:00 +0100

    renseigngemnt

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = $cleardb_url["eu-cdbr-west-03.cleardb.net"];
+$cleardb_username = $cleardb_url["b923c2350e2e98"];
+$cleardb_password = $cleardb_url["53598933"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
