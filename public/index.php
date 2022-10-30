fe4ebe41ae7261dcec117e7449bbdec07a1e7eda
guillaume <guillaume.laurent13880@gmail.com>
2022-10-30 21:09:45 +0100

    z

--- public/index.php
+++ public/index.php
@@ -1,9 +1,1 @@
-<?php
-
-use App\Kernel;
-
-require_once dirname(DIR).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+coucou
