dcb27b6157e55e6e3ae7ffc56e6f45856c88fe06
guillaume <guillaume.laurent13880@gmail.com>
2022-10-31 16:33:00 +0100

    correction

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 
-//$dbHost = 'eu-cdbr-west-03.cleardb.net';
-//$dbBase = 'heroku_bef80f3743ee9c8';
-//$dbUsername = 'b923c2350e2e98';
-//$dbPass = '53598933';
+$dbHost = 'eu-cdbr-west-03.cleardb.net';
+$dbBase = 'heroku_bef80f3743ee9c8';
+$dbUsername = 'b923c2350e2e98';
+$dbPass = '53598933';
