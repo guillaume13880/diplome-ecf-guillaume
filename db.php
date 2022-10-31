d8d4c742a47db21d15da5cd74dbf4bf412ed5ad8
guillaume <guillaume.laurent13880@gmail.com>
2022-10-31 12:07:26 +0100

    hello

--- db.php
+++ db.php
@@ -1,4 +1,16 @@
 <?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
 //Connexion au serveur
 define('DB_HOST', 'localhost:3306');
 //define('DB_PORT', '3306');
