6ec2bdd2bf74683c065f44b1906510b967a7a90e
guillaume <guillaume.laurent13880@gmail.com>
2022-10-31 10:48:58 +0100

    test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
  
 session_start();
