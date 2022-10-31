18a0a3a94f67178dce7c1ad99019a067cdc793e2
guillaume <guillaume.laurent13880@gmail.com>
2022-10-31 12:24:16 +0100

    erre

--- db.php
+++ db.php
@@ -8,21 +8,21 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$database = new PDO($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 //Connexion au serveur
-//define('DB_HOST', 'localhost:3306');
+define('DB_HOST', 'localhost:3306');
 //define('DB_PORT', '3306');
-//define('DB_DATABASE', 'ecf-graduate-2');
-//define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ecf-graduate-2');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 
 
 //création d'une instance de la classe PDO avec les paramètre de connexion définis
-//$database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+$database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 
 //si on a une erreur renvoie une exeption
- //$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   
