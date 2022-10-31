9721da5bc2b5488dad2351bd2556470b3b49f5f1
guillaume <guillaume.laurent13880@gmail.com>
2022-10-31 13:07:20 +0100

    hep

--- db.php
+++ db.php
@@ -1,28 +1,28 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
 // Connect to DB
-$database = new PDO($cleardb_server, $cleardb_username, $cleardb_password);
+//$database = new PDO($cleardb_server, $cleardb_username, $cleardb_password);
 
 
 //Connexion au serveur
-//define('DB_HOST', 'localhost:3306');
-//define('DB_PORT', '3306');
-//define('DB_DATABASE', 'ecf-graduate-2');
-//define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', '');
+define('DB_HOST', 'eu-cdbr-west-03.cleardb.net');
+define('DB_PORT', '3306');
+define('DB_DATABASE', 'heroku_bef80f3743ee9c8');
+define('DB_USERNAME', 'b923c2350e2e98');
+define('DB_PASSWORD', '53598933');
 
 
 //création d'une instance de la classe PDO avec les paramètre de connexion définis
-//$database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+$database = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 
 //si on a une erreur renvoie une exeption
- //$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   
