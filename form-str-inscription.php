d7fe2512595a05c2bb7cbba1a8c098e9cadc0b97
guillaume <guillaume.laurent13880@gmail.com>
2022-11-01 13:41:33 +0100

    rajout responsive

--- form-str-inscription.php
+++ form-str-inscription.php
@@ -131,13 +131,19 @@ if (!isset($_SESSION['verif'])) {
         @media screen and (max-width: 600px) {
             .login-form {
             width: 340px;
-            margin: 15vh auto;
-            
-        }
-        }
-       
-    </style>
-        
+            margin: 15vh auto;   
+            }
+             h1 {
+               font-size: 1.2rem; 
+            }
+            .btn {
+                font-size: 0.8rem;
+            }
+            .nav-item {
+                font-size: 0.8rem;
+            }
+        }    
+    </style>      
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
